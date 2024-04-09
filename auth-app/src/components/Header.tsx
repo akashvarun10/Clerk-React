bc1bb826a8d88ae783d0225e9c80e0a4c85ec88f
Pemmaraju Venkat Akash Varun <pemmaraju.v@northeastern.edu>
2024-04-09 12:05:47 -0400

    updated few files

--- auth-app/src/components/Header.tsx
+++ auth-app/src/components/Header.tsx
@@ -1,58 +1,5 @@
-// import { SignedIn, SignedOut, UserButton, useClerk } from "@clerk/clerk-react";
-// import { FC } from "react";
-// import { Link } from "react-router-dom"; // Import Link from react-router-dom
-// // import "../styles/Header.css";
-
-// const SignUpButton: FC = () => {
-//   const clerk = useClerk();
-
-//   return (
-//     <button className="sign-up-btn" onClick={() => clerk.openSignUp({})}>
-//       Sign up
-//     </button>
-//   );
-// };
-
-// const SignInButton: FC = () => {
-//   const clerk = useClerk();
-
-//   return (
-//     <button className="sign-in-btn" onClick={() => clerk.openSignIn({})}>
-//       Sign in
-//     </button>
-//   );
-// };
-
-// const Header: FC = () => {
-//   return (
-//     <header>
-//       <nav>
-//         <SignedOut>
-//           <ul>
-//             <li>
-//               <SignUpButton />
-//             </li>
-
-//             <li>
-//               <SignInButton />
-//             </li>
-//           </ul>
-//         </SignedOut>
-
-//         <SignedIn>
-//           <UserButton afterSignOutUrl="/" />
-//         </SignedIn>
-//       </nav>
-//       <Link to="/some-route">Some Route</Link> {/* Example usage of Link */}
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-import { AppBar, Toolbar, Button, Link as MuiLink } from '@mui/material';
+import { AppBar, Toolbar, Button } from '@mui/material';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
 import { SignedIn, SignedOut, UserButton, useClerk } from '@clerk/clerk-react';
 
 const SignUpButton: FC = () => {
@@ -79,11 +26,6 @@ const Header: FC = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Link to="/" style={{ flexGrow: 1 }}>
-          <MuiLink color="inherit" underline="none">
-            Clerk + React
-          </MuiLink>
-        </Link>
         <SignedOut>
           <SignUpButton />
           <SignInButton />
--- auth-app/src/main.tsx
+++ auth-app/src/main.tsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { ClerkProvider } from "@clerk/clerk-react";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import App from "./App";
-// // import Header from "./components/Header";
-// import Layout from "./components/Layout";
-// import NoMatch from "./components/NoMatch";
-
-// // import "./styles/index.css";
-
-// const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string;
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <ClerkProvider publishableKey={publishableKey}>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Layout />}>
-//             <Route index element={<App />} />
-//           </Route>
-//           <Route path="*" element={<NoMatch />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </ClerkProvider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ClerkProvider } from '@clerk/clerk-react';
